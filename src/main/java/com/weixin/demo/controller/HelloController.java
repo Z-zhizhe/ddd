238c92fcbffd0ab2e2ea8b4a36887e1debc7925b
Z-rongyin <1422072559@qq.com>
2019-06-10 20:12:50 +0800

    hellocontroller测试

--- src/main/java/com/weixin/demo/controller/HelloController.java
+++ src/main/java/com/weixin/demo/controller/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
     {
         System.out.println("Hello,Triple2");
         System.out.println("Hello，101");
-
+        System.out.println("20:12");
         return "Hello,Triple2";
     }
 }
