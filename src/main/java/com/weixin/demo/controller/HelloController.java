ddc0b68166e60685a9361189d2da5c0a0099fd55
Z-rongyin <1422072559@qq.com>
2019-06-10 16:46:53 +0800

    updata

--- src/main/java/com/weixin/demo/controller/HelloController.java
+++ src/main/java/com/weixin/demo/controller/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     public String Hello()
     {
         System.out.println("Hello,Triple2");
-        System.out.println("test");
+
         return "Hello,Triple2";
     }
 }
