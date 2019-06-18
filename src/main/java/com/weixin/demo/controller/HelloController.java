b21c0e7261aa614f7f32ac941d0315d1d398e0e2
QiaoShuTong <527922583@qq.com>
2019-06-18 08:46:52 +0800

    qst

--- src/main/java/com/weixin/demo/controller/HelloController.java
+++ src/main/java/com/weixin/demo/controller/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     public String Hello()
     {
         System.out.println("Hello,Triple2");
-
+        System.out.println("aaaaaaaa");
 
         return "Hello,Triple2";
     }
